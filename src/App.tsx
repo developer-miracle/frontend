cbc9dd584440c93f1b72d0fc899c303d8931bf85
devMiracle <rjcvjchzljv9@gmail.com>
2022-11-22 15:13:41 +0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Home from './components/pages/Home'
+import SignUp from './components/pages/SignUp'
+import SignIn from './components/pages/SignIn'
+import { Route, Routes } from 'react-router'
+import { Link, BrowserRouter as Router } from 'react-router-dom'
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/signin">signin</Link>
+          <Link to="/signup">signup</Link>
+        </nav>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/signin' element={<SignIn />}></Route>
+          <Route path='/signup' element={<SignUp />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/pages/SignUp.tsx
@@ -0,0 +1,8 @@
+export default function SignUp() {
+    return (
+        <div>
+            <h2>SignUp</h2>
+        </div>
+    )
+}
+
